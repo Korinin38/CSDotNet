edaf4ab8c6646c4b148fce1738e8c0a644b4accb
Korinin38 <artkash@bk.ru>
2022-09-21 15:45:39 +0300

    Lab 2

---
+++ Lab3/Simplifier.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CSDotNet.Lab3
+{
+    public class Simplifier
+    {
+        string Simplify(string arg)
+        {
+
+            return "";
+        }
+    }
+}
