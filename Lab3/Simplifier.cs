9e08812e6e0635acfff8946ce744b5f524ebc330
Korinin38 <artkash@bk.ru>
2022-09-27 14:13:45 +0300

    Lab3

--- Lab3/Simplifier.cs
+++ Lab3/Simplifier.cs
@@ -6,10 +6,25 @@ namespace CSDotNet.Lab3
 {
     public class Simplifier
     {
-        string Simplify(string arg)
+        private static int GCD(int a, int b)
         {
+            while (a != 0 && b != 0)
+            {
+                if (a > b)
+                    a %= b;
+                else
+                    b %= a;
+            }
 
-            return "";
+            return a | b;
+        }
+        public static string Simplify(string arg)
+        {
+            string[] subs = arg.Split('/');
+            int a = int.Parse(subs[0]);
+            int b = int.Parse(subs[1]);
+            int gcd = GCD(a, b);
+            return (a / gcd).ToString() + '/' + (b / gcd).ToString();
         }
     }
 }
